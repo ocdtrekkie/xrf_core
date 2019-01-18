f82eff56e0ed60dd3c909f3f44a56ee960574a6e
Jacob Weisz <inbox@jacobweisz.com>
2019-01-17 19:20:16 -0600

    Fix get functions
    I actually did this yesterday while my site over to 0.3a.

--- includes/functions_get.php
+++ includes/functions_get.php
@@ -2,41 +2,41 @@
 
 //Function xrf_get_fname
 //Use: Gets first name from a user id
-function xrf_get_fname($uid)
+function xrf_get_fname($xrf_db, $uid)
 {
 $query="SELECT fname FROM g_users WHERE id='$uid'";
-$result=mysql_query($query);
-$fname=mysql_result($result,$qq,"fname");
+$result=mysqli_query($xrf_db, $query);
+$fname=xrf_mysql_result($result,0,"fname");
 return ($fname);
 }
 
 //Function xrf_get_lname
 //Use: Gets last name from a user id
-function xrf_get_lname($uid)
+function xrf_get_lname($xrf_db, $uid)
 {
 $query="SELECT lname FROM g_users WHERE id='$uid'";
-$result=mysql_query($query);
-$lname=mysql_result($result,$qq,"lname");
+$result=mysqli_query($xrf_db, $query);
+$lname=xrf_mysql_result($result,0,"lname");
 return ($lname);
 }
 
 //Function xrf_get_profilepic
 //Use: Gets profile picture URL from a user id
-function xrf_get_profilepic($uid)
+function xrf_get_profilepic($xrf_db, $uid)
 {
 $query="SELECT profilepic FROM g_users WHERE id='$uid'";
-$result=mysql_query($query);
-$profilepic=mysql_result($result,$qq,"profilepic");
+$result=mysqli_query($xrf_db, $query);
+$profilepic=xrf_mysql_result($result,0,"profilepic");
 return ($profilepic);
 }
 
 //Function xrf_get_username
 //Use: Gets username from a user id
-function xrf_get_username($uid)
+function xrf_get_username($xrf_db, $uid)
 {
 $query="SELECT username FROM g_users WHERE id='$uid'";
-$result=mysql_query($query);
-$username=mysql_result($result,$qq,"username");
+$result=mysqli_query($xrf_db, $query);
+$username=xrf_mysql_result($result,0,"username");
 return ($username);
 }
 
