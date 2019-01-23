3b2f2cfcbbf4b204ae5bcac0db93907f5ce1a777
Jacob Weisz <inbox@jacobweisz.com>
2019-01-22 22:50:41 -0600

    Remove long unused test files
    Really should never have brought them over in the first place.

--- test_encryptpassword.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("includes/global.php");
-require_once("includes/functions_auth.php");
-
-$new = encrypt_password($encrypt, $passwordsalt);
-echo $new;
-
-require_once("includes/footer.php");
-?>
--- test_generatepassword.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("includes/global.php");
-require_once("includes/functions_auth.php");
-
-$new = generate_password($length);
-echo $new;
-
-require_once("includes/footer.php");
-?>
