a59f5e4f5e8c163d43fde48ae52f19fbe42c7269
ocdtrekkie <inbox@jacobweisz.com>
2021-04-27 15:42:50 -0500

    Add base print style

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@ if ($xrf_mystylepref == "") {$xrf_style = $xrf_style_default;}
 else {$xrf_style = $xrf_mystylepref;}
 echo "<html><head><title>$xrf_site_name Account</title>
 <link rel=\"stylesheet\" type=\"text/css\" href=\"styles/$xrf_style/style.css\" />
+<link rel=\"stylesheet\" type=\"text/css\" href=\"styles/print/style.css\" media=\"print\" />
 </head><body>";
 
 if ($xrf_myid != 0)
@@ -19,10 +20,10 @@ echo "<div class=\"header\" align=\"center\">
 <table width=\"100%\"><tr><td>
 <p align=\"left\">
 <font size=\"6\">$xrf_site_name Account</font><br>
-$xrf_myusername
+<span class=\"navigation-box\">$xrf_myusername</span>
 </p>
 </td><td>
-<p align=\"right\">
+<p align=\"right\" class=\"navigation-box\">
 $navbox$sstatus
 </p>
 </td></tr></table>
